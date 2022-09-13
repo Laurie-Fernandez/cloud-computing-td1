6511bdae598ffb24160b95e475ce1b7e7885c5fb
Laurie Fernandez <laurie.fernandez@etu.unice.fr>
2022-09-13 17:31:19 +0200

    Fin partie PaaS-on au Cloud

--- server.js
+++ server.js
@@ -2,20 +2,27 @@
 const express = require('express')
 const app = express()
 const port = process.env.PORT || 3000;
+const db_url = process.env.DATABASE_URL || "postgres://"+process.env.POSTGRES_USER+":"+process.env.POSTGRES_PASSWORD+"@"+process.env.POSTGRES_HOST+":"+5432+"/"+process.env.POSTGRES_DB;
 var pgp = require("pg-promise")(/*options*/);
-var db = pgp("postgres://"+process.env.POSTGRES_USER+":"+process.env.POSTGRES_PASSWORD+"@"+process.env.POSTGRES_HOST+":"+port+"/"+process.env.POSTGRES_DBT);
+var db = pgp(db_url);
 
 app.get('/', (req, res) => {
-    res.send('Hello World!')
+    db.query("SELECT * from hello")
+        .then(function (data) {
+            let counter = data[0].counter;
+            counter = counter + 1;
+            db.query(`update hello set counter=${counter} where id=0`)
+                .catch(function (error) {
+                    console.log("ERROR:", error);
+                });
+            res.send(`Hello World! x${counter}`);
+        })
+        .catch(function (error) {
+            console.log("ERROR:", error);
+        });
 })
 
-db.query("SELECT $1 AS value", 123)
-    .then(function (data) {
-        console.log("DATA:", data.value);
-    })
-    .catch(function (error) {
-        console.log("ERROR:", error);
-    });
+db.query("drop table if exists hello; create table hello(id int primary key, counter int); insert into hello values(0,0);");
 
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
